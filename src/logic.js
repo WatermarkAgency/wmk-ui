7fbe039d0726dd0e2756250b8a4c42db08c8988f
Peter Washington <peter@wmkagency.com>
2022-04-20 15:10:55 -0600

    init v2 package w/ button

--- src/logic.js
+++
@@ -1,8 +0,0 @@
-export const wmkClass = (name, group, append = "", prefix = "wmk") => {
-    const classes = [
-      prefix + "-" + group,
-      prefix + "-" + group + "-" + name,
-      append
-    ];
-    return classes.join(" ").trim();
-  };
